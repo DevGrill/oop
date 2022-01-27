6475307e696b7c1e6c6663b2374081dbfdf292f1
Grilly <cyril.kurmann@gmail.com>
2022-01-27 13:50:34 +0100

    update

--- wahrenkorb/src/main/java/me/devgrill/wahrenkorb/WahrenKorbController.java
+++ wahrenkorb/src/main/java/me/devgrill/wahrenkorb/WahrenKorbController.java
@@ -2,11 +2,14 @@ package me.devgrill.wahrenkorb;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.util.LinkedList;
 
@@ -38,12 +41,24 @@ public class WahrenKorbController {
             orderedItems.add(item);
             amount++;
             anzahl.setText("(" + Integer.toString(amount) + ")");
-            for (int i=0; i<orderedItems.size(); i++)
-            {
-                System.out.println(orderedItems.get(i));
-            }
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    void showWarenkorb(ActionEvent event){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("cart-view.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+            OrdersController o = new OrdersController();
+            o.insertOrders(orderedItems);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
